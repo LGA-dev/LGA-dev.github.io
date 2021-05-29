0c3362ae7702bd50de10890d5cf5969006289b4c
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-05-29 09:13:28 -0300

    Update ProjectCard component style

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,24 +1,35 @@
 import { Button } from "@chakra-ui/button";
-import { Link } from "@chakra-ui/layout";
-import { Box, Heading, Text, Flex, Stack } from "@chakra-ui/layout";
-import { Image, Icon } from "@chakra-ui/react"
+import { Flex, Heading, Stack, Text } from "@chakra-ui/layout";
+import { Icon, Image } from "@chakra-ui/react";
 
 const ProjectCard = ({projectName, projectDescription, imageURL, techStack}) => {
   // console.log(techStack)
   return (
-    <Stack spacking={5} bg="white" borderWidth="1px" borderRadius="lg" boxShadow="md" p="6" mt="4" rounded="md">
+    <Stack
+      spacing={6}
+      p={6}
+      mt={3}
+      borderWidth="1px"
+      borderRadius="lg"
+      boxShadow="base"
+      rounded="md"
+      background="#ffffff"
+    >
       <Heading size="lg">{projectName}</Heading>
       <Image src={imageURL}/>
       <Text fontSize="md">{projectDescription}</Text>
-      <Flex justifyContent="space-between" alignItems="center" bg="tomato">
+      <Flex justifyContent="center">
+        {techStack && techStack.map((tech) => {
+          return <Icon boxSize={12} as={ tech }/>
+        })}
+      </Flex>
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Button>Demo online</Button>
         <Button>Código fuente</Button>
       </Flex>
-      <Flex justifyContent="center">
-          {techStack && techStack.map((tech) => {
-            return <Icon boxSize="3rem" as={ tech }/>
-          })}
-        </Flex>
     </Stack>
   );
 }
