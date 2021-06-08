2c44a58601bfdde3c4964db37c5ab51f9f2bc3ad
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-08 10:06:39 -0300

    Create ProfileIcon component

---
+++ src/components/ProfileIcon.js
@@ -0,0 +1,35 @@
+import { Box, Center, Flex, GridItem, Heading, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import { FaCaretSquareDown, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { Link as ReactLink } from "react-router-dom";
+
+const ProfileIcon = ({url, icon, iconDescription}) => {
+  return (
+    <Link
+      width={24}
+      href={url}
+    >
+      <Center flexDirection="column">
+        {/* Square container for Icon */}
+        <Box
+          p={3}
+          borderRadius="lg"
+          backgroundColor="#7e57c2"
+        >
+          <Icon
+            boxSize={12}
+            as={icon}
+          />
+        </Box>
+        {/* Icon name */}
+        <Text
+          mt={1}
+          fontWeight="600"
+        >
+          {iconDescription}
+        </Text>
+      </Center>
+    </Link>
+  );
+}
+
+export default ProfileIcon;
