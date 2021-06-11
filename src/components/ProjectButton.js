50edc39b8f243cd4cbba64c5cff40f104f279315
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-10 21:15:15 -0300

    Add HandlerContext to ProjectButton component

--- src/components/ProjectButton.js
+++ src/components/ProjectButton.js
@@ -1,11 +1,15 @@
 import { Button } from "@chakra-ui/button";
 import { Box } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/react";
+import { useContext } from "react";
+import { HandlerContext } from "../context/HandlerContext";
 
 const ProjectButton = ({icon, iconDescription}) => {
+  const { handleActiveStack } = useContext(HandlerContext)
+
   return (
     <Box>
-      <Button position="static">
+      <Button position="static" onClick={() => handleActiveStack(iconDescription)}>
         <Image w={6} mr={2} src={icon}></Image>
         {iconDescription}
       </Button>
