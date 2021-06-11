a34d4511a96b68bed219d1a3f451f785804215a0
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-06-10 21:12:34 -0300

    Create handleActiveStack function in HandlerContext

--- src/context/HandlerContext.js
+++ src/context/HandlerContext.js
@@ -3,14 +3,20 @@ import { createContext, useState } from "react";
 export const HandlerContext = createContext();
 
 export const HandlerProvider = (props) => {
+  // Navbar Status Handler
   let [isOpen, setIsOpen] = useState(false)
   const handleOpen = (boolean) => {
     setIsOpen(boolean)
   }
-  // console.log(isOpen)
+
+  // Active Stack Handler
+  let [activeStack, setActiveStack] = useState("");
+  const handleActiveStack = (stack) => {
+    setActiveStack(stack)
+  }
   
   return (
-    <HandlerContext.Provider value={{isOpen, handleOpen}}>
+    <HandlerContext.Provider value={{isOpen, handleOpen, activeStack, handleActiveStack}}>
       {props.children}
     </HandlerContext.Provider>
   )
